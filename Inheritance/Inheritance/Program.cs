e358125e7ff67b63b0942f86887d1895c6bf9df0
Mark <markknight2409boeing@gmail.com>
2023-04-26 10:59:01 +0300

    1

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -10,6 +10,64 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            Warrior character1 = new Warrior("Jack", 100, 25, 50);
+            character1.ShowInfo();
+
+            Console.WriteLine(  );
+
+            Archer character2 = new Archer("Bob", 75, 35);
+            character2.ShowInfo();           
+        }
+    }
+
+    public class Character
+    {
+        private string _name;
+        private int _health;
+        private int _damage;
+
+        public Character(string name, int health, int damage)
+        {
+            _name = name;
+            _health = health;
+            _damage = damage;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {_name}\nHealth: {_health}\nDamage: {_damage}");
+        }
+    }
+
+    public class Warrior : Character
+    {
+        private int _defRate;
+
+        public Warrior(string name, int health, int damage, int defRate) : base(name, health, damage)
+        {
+            _defRate = defRate;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Def rate: {_defRate}");
+        }
+    }
+
+    public class Archer : Character
+    {
+        private int _arrowsCount;
+
+        public Archer(string name, int health, int damage) : base(name, health, damage)
+        {
+            _arrowsCount = 30;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Arrows left: {_arrowsCount}");
         }
     }
 }
